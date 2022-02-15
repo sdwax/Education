d6ff077f9b31216ec5a148db3aadc6cc2c562938
Vasyl Kachan <v.kachan@spd-ukraine.com>
2022-02-15 09:57:42 +0200

    Modifier insertion sort (break iterate if array sorted)

--- InsertionSort/src/main/java/com/InsertionSorter.java
+++ InsertionSort/src/main/java/com/InsertionSorter.java
@@ -2,11 +2,14 @@ package com;
 
 public class InsertionSorter {
     public <T extends Comparable<T>> void sort(T[] array) {
+        boolean needIterable = true;
         for (int i = 1; i < array.length; i++) {
             if (array[i - 1].compareTo(array[i]) > 0) {
-                for (int j = i; j > 0; j--) {
+                for (int j = i; j > 0 && needIterable; j--) {
+                    needIterable = false;
                     if (array[j - 1].compareTo(array[j]) > 0) {
                         swap(array, j - 1, j);
+                        needIterable = true;
                     }
                 }
             }
